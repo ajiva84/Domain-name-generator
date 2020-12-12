8da70d6f38bf0bd0a9c5b73126731d7cecb4799f
ajiva84 <jivazam@gmail.com>
2020-12-12 04:26:05 +0000

    with condition

--- src/main.js
+++ src/main.js
@@ -6,17 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onclick = function() {
-  console.log("Hello Rigo from the console!");
-
-  //   let mybuttonread = document.getElementById("mybuttonread");
-  //   mybuttonread.addEventListener("click", list);
-
-  console.log("Hello from the console Anystring");
-
-  //   var suit = ["heart", "club", "spade", "diamond"];
-  //   var value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-
-  //   function list() {
   var suit = ["heart", "club", "spade", "diamond"];
   var value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 
@@ -27,8 +16,12 @@ window.onclick = function() {
   var card = document.querySelector(".card");
   var cardValue = document.querySelector("h1");
 
+  for (let key in suit) {
+    if (card.classList.contains(suit[key])) {
+      card.classList.remove(suit[key]);
+    }
+  }
+
   card.classList.add(suit[getRndInteger(0, suit.length - 1)]);
   cardValue.innerHTML = value[getRndInteger(0, value.length - 1)];
-  console.log("I am here");
-  //   }
 };
